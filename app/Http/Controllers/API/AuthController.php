7bb6e74c46c8b091b0fedc3c54ea7973872819d4
Alexander Dmitriev <alex1998dmit@gmail.com>
2019-10-31 12:40:25 +0300

    Test commit

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -30,26 +30,35 @@ class AuthController extends Controller
         }
 
         $https = new \GuzzleHttp\Client;
-        try {
-            $response = $https->post(env('APP_HOST') . '/oauth/token', [
-                'form_params' => [
-                    'grant_type' => 'password',
-                    'client_id' => 2,
-                    'client_secret' => env('APP_SECRET'),
-                    'username' => $request->username,
-                    'password' => $request->password,
-                ]
-            ]);
-            return $response->getBody();
-        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-            dd($e);
-            if ($e->getCode() === 400) {
-                return response()->json('Invalid Request. Please enter a username or a password.', $e->getCode());
-            } else if ($e->getCode() === 401) {
-                return response()->json('Your credentials are incorrect. Please try again', $e->getCode());
-            }
-            return response()->json('Something went wrong on the server.', $e->getCode());
-        }
+        $response = $https->post(env('APP_HOST') . '/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => 2,
+                'client_secret' => env('APP_SECRET'),
+                'username' => $request->username,
+                'password' => $request->password,
+            ]
+        ]);
+        return $response->getBody();
+//        try {
+//            $response = $https->post(env('APP_HOST') . '/oauth/token', [
+//                'form_params' => [
+//                    'grant_type' => 'password',
+//                    'client_id' => 2,
+//                    'client_secret' => env('APP_SECRET'),
+//                    'username' => $request->username,
+//                    'password' => $request->password,
+//                ]
+//            ]);
+//            return $response->getBody();
+//        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+//            if ($e->getCode() === 400) {
+//                return response()->json('Invalid Request. Please enter a username or a password.', $e->getCode());
+//            } else if ($e->getCode() === 401) {
+//                return response()->json('Your credentials are incorrect. Please try again', $e->getCode());
+//            }
+//            return response()->json('Something went wrong on the server.', $e->getCode());
+//        }
     }
 
     public function register(Request $request)
