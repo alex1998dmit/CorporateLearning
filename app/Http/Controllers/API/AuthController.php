8f5491a8df5f4012529f40fa0019dd3ee9a0f074
Alexander Dmitriev <alex1998dmit@gmail.com>
2019-10-31 12:46:46 +0300

    test commit

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -30,6 +30,13 @@ class AuthController extends Controller
         }
 
         $https = new \GuzzleHttp\Client;
+        $response = $https->get(env('APP_HOST') . '/oauth/token', [
+            'form_params' => [
+                'client_secret' => env('APP_SECRET'),
+                'message' => $request->test_message
+            ]
+        ]);
+        return $response->getBody();
         $response = $https->post(env('APP_HOST') . '/oauth/token', [
             'form_params' => [
                 'grant_type' => 'password',
